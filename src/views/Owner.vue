2bd78d68ddf0ccba95b6f0c7fa4263cf36856dec
aimeliala <idstack@outlook.co.id>
2023-12-21 00:44:42 +0700

    membuat nested component dengan router

---
+++ src/views/Owner.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRoute } from "vue-router";
+import products from "../data/products.json";
+
+const route = useRoute();
+const { owner } = products.find((p) => p.id === parseInt(route.params.id));
+</script>
+
+<template>
+  <div>
+    <h1>Owner</h1>
+    <p>{{ owner.name }}</p>
+    <p>{{ owner.email }}</p>
+  </div>
+</template>
