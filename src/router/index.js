100e91625bf6bba837974ffa1060dca10d3eeb32
aim <ahmadhakim404@gmail.com>
2024-01-13 09:50:54 +0700

    cara mengatasi halaman tidak ditemukan

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../views/Home.vue"),
-    },
-    {
-      path: "/products/:id",
-      name: "products",
-      component: () => import("../views/Product.vue"),
-      children: [
-        {
-          path: "owner",
-          name: "owner",
-          component: () => import("../views/Owner.vue"),
-        },
-      ],
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/About.vue"),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: () => import('../views/Home.vue'),
+		},
+		{
+			path: '/products/:id',
+			name: 'products',
+			component: () => import('../views/Product.vue'),
+			children: [
+				{
+					path: 'owner',
+					name: 'owner',
+					component: () => import('../views/Owner.vue'),
+				},
+			],
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: () => import('../views/About.vue'),
+		},
+		{
+			path: '/:catchall(.*)*',
+			name: 'Not Found',
+			component: () => import('../views/errors/404.vue'),
+		},
+	],
 });
 
 export default router;
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useRoute, useRouter, RouterView } from "vue-router";
-import products from "../data/products.json";
+import { useRoute, useRouter, RouterView } from 'vue-router';
+import products from '../data/products.json';
 
 const route = useRoute();
 const router = useRouter();
@@ -9,18 +9,21 @@ const productId = parseInt(route.params.id);
 const product = products.find((product) => product.id === productId);
 
 function showOwner() {
-  router.push({ name: "owner", params: { id: productId } });
+	router.push({ name: 'owner', params: { id: productId } });
 }
 </script>
 
 <template>
-  <div>
-    <h1>Product</h1>
-    <div>
-      <h2>{{ product.name }} - {{ product.price }}</h2>
-      <p>Year: {{ product.year }}</p>
-    </div>
-    <button @click="showOwner">Show Owner</button>
-    <RouterView />
-  </div>
+	<div v-if="product">
+		<h1>Product</h1>
+		<div>
+			<h2>{{ product.name }} - {{ product.price }}</h2>
+			<p>Year: {{ product.year }}</p>
+		</div>
+		<button @click="showOwner">Show Owner</button>
+		<RouterView />
+	</div>
+	<div v-else>
+		<h1>Product not found</h1>
+	</div>
 </template>
